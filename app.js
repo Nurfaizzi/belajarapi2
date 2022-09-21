1b02720770651db841343cf77264485ff99b05dd
Nurfaizzi <113836263+Nurfaizzi@users.noreply.github.com>
2022-09-21 23:09:28 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+ 
+
+// basic route
 app.get('/', (req, res) => {
-  res.send('Hello Rasyid!')
+    res.send('Hello World');
 })
 
-app.listen(port, () => {
-  console.log(`App Is running with port ${port}`)
+//About
+app.get('/About', (req, res) => {
+    res.send('Hello ');
 })
+
+
+//Contac
+app.get('/Contac', (req, res) => {
+    res.send('08222529320');
+})
+app.listen(3000, () => console.log('Server Running at http://localhost:3000'))
